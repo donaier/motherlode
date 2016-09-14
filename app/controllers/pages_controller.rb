b7c2ce70f2c43dd9d3ac05cecd21f0cdecd7a4b1
Donat Baier <donat.baier@gmail.com>
2016-09-14 23:54:03 +0200

    ja

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
 
   def gallery
     @images = Image.all.order(:position)
-    @photos = Dir.entries('app/assets/images/gallery') - ['.', '..', '.DS_Store']
   end
 end
