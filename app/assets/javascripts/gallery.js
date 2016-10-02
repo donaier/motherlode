20e3aa0f44743afab03541ad48cdec745c91a476
Donat Baier <donat.baier@gmail.com>
2016-10-02 19:30:26 +0200

    minus turbolinks

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,6 +1,6 @@
-$(window).load(function() {
+$(window).on('load', (function() {
   // mebe trigger this on resize?
   $('.gallery').each(function(i, obj) {
     $(obj).BalancedGallery({ idealHeight: 350 });
   });
-});
+}));
