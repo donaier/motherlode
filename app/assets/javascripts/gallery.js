4cd83376eb6644d100a3f43a0a792d775c80c661
donaier <donaier@undrhund.local>
2016-10-30 17:25:29 +0100

    trigger the shit on some event

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,6 +1,8 @@
 $(window).on('load', (function() {
   // mebe trigger this on resize?
-  $('.gallery').each(function(i, obj) {
-    $(obj).BalancedGallery({ idealHeight: 350 });
+  imagesLoaded( document.querySelector('#gallery'), function( instance ) {
+    $('.gallery').each(function(i, obj) {
+      $(obj).BalancedGallery({ idealHeight: 350 });
+    });
   });
 }));
