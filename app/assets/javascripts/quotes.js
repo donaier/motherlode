382c35302c3ece19562fe9f316b511205a233a12
donaier <donaier@undrhund.local>
2016-11-03 10:20:01 +0100

    ted tile

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -1,15 +1,15 @@
-$(document).ready( function() {
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-    .done(update)
-    .fail(handleErr);
-});
-
-function update(response) {
-  var quote = JSON.parse(JSON.stringify(response));
-  $('#quote').html('"' + quote.quoteText + '"');
-  $('#author').html("– " + quote.quoteAuthor);
-}
-
-function handleErr(jqxhr, textStatus, err) {
-  console.log("Request Failed: " + textStatus + ", " + err);
-}
+// $(document).ready( function() {
+//   $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+//     .done(update)
+//     .fail(handleErr);
+// });
+//
+// function update(response) {
+//   var quote = JSON.parse(JSON.stringify(response));
+//   $('#quote').html('"' + quote.quoteText + '"');
+//   $('#author').html("– " + quote.quoteAuthor);
+// }
+//
+// function handleErr(jqxhr, textStatus, err) {
+//   console.log("Request Failed: " + textStatus + ", " + err);
+// }
