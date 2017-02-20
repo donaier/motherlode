b0dfc50bd1c1549c2f19bd3acb27778de72a42b7
donaier <donaier@undrhund.zhaw.ch>
2017-02-20 14:59:08 +0100

    moduli

--- app/models/calendar_entry.rb
+++ app/models/calendar_entry.rb
@@ -2,15 +2,7 @@ class CalendarEntry < ApplicationRecord
 
   scope :batches, -> { select(:date, :modul).distinct }
 
-  MODULI = ['1.1',
-            '1.2',
-            '2.1',
-            '2.2',
-            '3.1',
-            '3.2',
-            '4.1',
-            '4.2',
-            '5']
+  MODULI = ['1.1', '1.2', '2.1', '2.2', '3.1', '3.2', '4.1', '4.2', '5', '6.1', '7.1', '8', '9', '10', '11']
 
   def start_time
     self.date
