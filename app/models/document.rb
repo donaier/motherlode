22ad5cb453d30a2ee39e1d7d49b51e4eca26cfbf
donaier <donaier@undrhund.local>
2017-11-21 19:38:46 +0100

    merge moduli

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,20 +4,17 @@ class Document < ApplicationRecord
   has_attached_file :file
   validates_attachment_content_type :file, :content_type => 'application/pdf'
 
-  scope :archived, -> { where(modul: PAST_MODULI) }
-
-  MODULI = ['Modul 6 - rechtliche und ethische grundlagen',
+  MODULI = ['Modul 1 - geschichte/theorien',
+            'Modul 2 - psychologie',
+            'Modul 3 - professionelles zeug',
+            'Modul 4 - wissenschaftszeugs',
+            'Modul 5 - portfolio',
+            'Modul 6 - rechtliche und ethische grundlagen',
             'Modul 7 - rahmenbedingungen',
             'Modul 8 - wandel, probleme, ungleichheit',
             'Modul 9 - organisationen im sozialwesen',
             'Modul 10 - fallwerkstatt',
             'Modul 11 - portfolio 2']
-
-  PAST_MODULI = ['Modul 1 - geschichte/theorien',
-                 'Modul 2 - psychologie',
-                 'Modul 3 - professionelles zeug',
-                 'Modul 4 - wissenschaftszeugs',
-                 'Modul 5 - portfolio']
 
   private
 
